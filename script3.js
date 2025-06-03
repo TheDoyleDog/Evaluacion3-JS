20809fd8625ba5619a0b81ce4a51de8794515ef2
TheDoyleDog <doyleeeeegg@gmail.com>
2025-06-03 11:32:16 -0400

    Commit Final

--- script3.js
+++ script3.js
@@ -13,6 +13,40 @@ function validar(){
     if (vTelefono.length = 8){
         vTelefono.innerText=""
         eTelefono.style.backgroundColor="green"
-        eTelefono
+        eTelefono.style.color="white"
+    }else{
+        vTelefono.innerHTML="<strong>Debe ingresar un numero exactamente de 8 digitos</strong>"
+        eTelefono.style.backgroundColor="red"
+        eTelefono.style.color="white"
+        valido = false
     }
+
+    if (vPassword.length > 5){
+        vPassword.innerText=""
+        ePassword.style.backgroundColor="green"
+        ePassword.style.color="white"
+    }else{
+        vPassword.innerHTML="<strong>Debe ingresar una contraseña de mas de 5 caracteres</strong>"
+        ePassword.style.backgroundColor="red"
+        ePassword.style.color="white"
+        valido=false
+    }
+    
+}
+function cargarDatos(){
+
+}
+function eliminar(){
+    if usuarios.filter((p, index) =>{
+        if (index != indice){
+            return p
+        }   
+    })
+    cargarDatos()
+}
+function actualizar(){
+    let eTelefono = document.getElementById("telefono1")
+    let vTelefono = eTelefono.value
+    let ePassword = document.getElementById("password1")
+
 }
